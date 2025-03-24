c882357d3f54309f3ffc8f6a94765c24a1e97406
Muhamad Imam Catur <mimamc95@gmail.com>
2025-03-24 16:33:40 +0700

    add new data POST method

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const app = express()
 const port = 3333
 const router = require('./routes/index')
 
-
+// add middleware express for endpoint can accessing req.body with json
 app.use(express.json())
-app.use(router)
+
+// app.use(router)
+
 // array of object for representation of database students
 const students = [
     { id: 1, nama: 'Rega Jeatreya', nim: 160801, jurusan: 'TI' },
@@ -39,6 +41,30 @@ app.get('/students', (req, res) => {
 })
 
 
+// create router endpoint create new student
+app.post('/students', (req, res) => {
+    // get request body
+    const { nama, nim, jurusan } = req.body
+
+    // get new id student
+    const lastStudentId = students[students.length - 1].id
+    const newIdStudent = lastStudentId + 1
+
+    // add to new data student
+    const newStudentData = { id: newIdStudent, nama: nama, nim: nim, jurusan: jurusan }
+    students.push(newStudentData)
+
+    // return response to client
+    res.status(201).json({
+        status: 'Ok',
+        message: 'Success cretae new data student',
+        data: newStudentData
+
+    })
+})
+
+
+
 // create router endpoint  students for read data students by id
 app.get('/students/:id', (req, res) => {
     // console.log(req.params)
@@ -49,10 +75,18 @@ app.get('/students/:id', (req, res) => {
     // proccessing data or looping data student
     for (let i = 0; i < students.length; i++) {
         // if data student id === id on req.params, save / use that data 
-        if (students[i].id === Number(id)){
+        if (students[i].id === Number(id)) {
             student = students[i]
         }
-            
+
+    }
+
+    // if data student undifined, send status 404 not found
+    if (student == undefined) {
+        return res.status(404).json({
+            status: 'Failed',
+            message: `Data student with id ${id} is not found`
+        })
     }
 
     // return response to client
