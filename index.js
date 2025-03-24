f0996230fa3a634e4290a2265139b43510fe833b
Muhamad Imam Catur <mimamc95@gmail.com>
2025-03-24 11:28:10 +0700

    Read all data GET method

--- index.js
+++ index.js
@@ -3,18 +3,39 @@ const express = require('express')
 const app = express()
 const port = 3333
 
+// representation of database of students
+const students = [
+    { id: 1, nama: 'Rega Jeatreya', nim: 160801, jurusan: 'TI' },
+    { id: 2, nama: 'Adelia Putri', nim: 160802, jurusan: 'TI' },
+    { id: 3, nama: 'Prima Nugraha', nim: 160803, jurusan: 'TI' },
+    { id: 4, nama: 'Risma Noviana', nim: 160804, jurusan: 'TI' },
+    { id: 5, nama: 'Rifky Ananda', nim: 160805, jurusan: 'TI' },
+]
 
 app.get('/', (req, res) => {
-    // if want to return response with json
-    res.json({message:"hello world"})
+    // create content type res.json for produce api
+    res.json({ message: "hello world" })
 
-    // if want to return response with html
+    // create content type res.send for pruduce html
     // res.send('<h1>Hello World</h1>')
+})
+
+// create router students
+app.get('/students', (req, res) => {
+    // get data from data from database students
+    const dataStd = students
+
+    // return data with json 
+    const result = {
+        status: 'Ok',
+        data: dataStd
+    }
+    res.json(result)
 })
 
 app.get('/users/:id', (req, res) => {
     // console.log(req.params)
-    const {id} = req.params
+    const { id } = req.params
     res.send(`This is users with id ${id}`)
 })
 app.listen(port, () => console.log(`Server running on port ${port}`))
