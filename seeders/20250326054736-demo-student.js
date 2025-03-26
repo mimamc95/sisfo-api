a1bc8667416190a61bfbec5c4cc984f779953af0
Muhamad Imam Catur <mimamc95@gmail.com>
2025-03-26 12:59:41 +0700

    create seeders with sequelize-cli

---
+++ seeders/20250326054736-demo-student.js
@@ -0,0 +1,40 @@
+// Use to create first seed data on database
+
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    return queryInterface.bulkInsert('students', [
+      {
+        nama: 'Patrick',
+        nim: 160810,
+        jurusan: 'TI',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    return queryInterface.bulkDelete('students', null, {});
+  }
+};
