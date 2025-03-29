0ede517a3b0028a594b1182d2f0ac1ec136510a4
Muhamad Imam Catur <mimamc95@gmail.com>
2025-03-29 23:19:12 +0700

    create new usersRouter

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 // implementation plugin routes from express
 const router = require('express').Router()
 const studentsRouter = require('./studentsRouter')
+const usersRouter = require('./usersRouter')
 
 
 // refactor router for students
 router.use('/api/v1', studentsRouter)
+
+// refactor router for users
+router.use('/api/v1/users', usersRouter)
 
 
 // export file router from other file
