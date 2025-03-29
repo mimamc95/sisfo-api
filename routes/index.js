e3b5db704412248cb1b8d61686897b6ed759fc1d
Muhamad Imam Catur <mimamc95@gmail.com>
2025-03-29 19:36:23 +0700

    refactor using /api/v1 pattern

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,11 @@
-const { findAllStudent, getStudentbyId, createNewStudent, updateStudent, destroyStudent } = require('../controller/studentController')
-
 // implementation plugin routes from express
 const router = require('express').Router()
+const studentsRouter = require('./studentsRouter')
 
-// router READ all student, import data from studentController
-router.get('/students', findAllStudent)
 
-// router READ student by id, import data from studentController
-router.get('/students/:id', getStudentbyId)
+// refactor router for students
+router.use('/api/v1', studentsRouter)
 
-// router CREATE data student, import data from studentController
-router.post('/students', createNewStudent)
-
-// router UPDATE data student, import data from studentController
-router.patch('/students/:id', updateStudent)
-
-// router DELETE data student, import data from studentController
-router.delete('/students/:id', destroyStudent)
 
 // export file router from other file
 module.exports = router
---
+++ routes/studentsRouter.js
@@ -0,0 +1,23 @@
+// import Router
+const router = require('express').Router()
+
+const { findAllStudent, getStudentbyId, createNewStudent, updateStudent, destroyStudent } = require('../controller/studentController')
+
+
+// router READ all student, import data from studentController
+router.get('/students', findAllStudent)
+
+// router READ student by id, import data from studentController
+router.get('/students/:id', getStudentbyId)
+
+// router CREATE data student, import data from studentController
+router.post('/students', createNewStudent)
+
+// router UPDATE data student, import data from studentController
+router.patch('/students/:id', updateStudent)
+
+// router DELETE data student, import data from studentController
+router.delete('/students/:id', destroyStudent)
+
+// export file router from other file
+module.exports = router
