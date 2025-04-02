1e58929dc8f4b8b72d793bb27e03a3ad4599418d
Muhamad Imam Catur <mimamc95@gmail.com>
2025-04-02 22:49:51 +0700

    create endpoint findAllMakul, getMakulById

--- controller/mataKuliahController.js
+++ controller/mataKuliahController.js
@@ -34,5 +34,54 @@ const addMakul = async (req, res, next) => {
     }
 }
 
+// create router endpoint makul for create new makul 
+const findAllMakul = async (req, res, next) => {
+    try {
+        // get data from data from database MataKuliahs
+        const dataMakul = await MataKuliah.findAll()
+        // return result with json
+        const result = {
+            status: 'Ok',
+            data: dataMakul
+        }
+        res.json(result)
+
+    } catch (error) {
+        console.log(error, '<<< Error find all data Mata Kuliah')
+        // call middleware errHandler
+        next(error)
+    }
+
+}
+
+// create router endpoint makul for create new makul 
+const getMakulById = async (req, res, next) => {
+    try {
+        // get id from request params
+        const { id } = req.params
+        const dataMakul = await MataKuliah.findByPk(id)
+
+        // if data user null/undifined, send status 404 not found
+        if (dataMakul === null) {
+            return res.status(404).json({
+                status: 'Failed',
+                message: `Data with id ${id} is not found`
+            })
+        }
+        // return response to client with json/api
+        res.status(200).json({
+            status: 'Ok',
+            data: dataMakul
+        })
+
+    } catch (error) {
+        console.log(error, '<<< Error get data Mata Kuliah by id')
+        // call middleware errHandler
+        next(error)
+    }
+
+}
+
+
 // export controller functions so they can be accessed in other files
-module.exports = { addMakul }
+module.exports = { addMakul, findAllMakul, getMakulById }
--- routes/mataKuliahRouter.js
+++ routes/mataKuliahRouter.js
@@ -1,11 +1,16 @@
 // import Router
 const router = require('express').Router()
 
-const { addMakul } = require('../controller/mataKuliahController')
+const { addMakul, findAllMakul, getMakulById } = require('../controller/mataKuliahController')
 
 
 // router CREATE data makul, import data from mataKuliahController
-
 router.post('/', addMakul)
+
+// router READ all data makul, import data from mataKuliahController
+router.get('/', findAllMakul)
+
+// router READ data makul by id, import data from mataKuliahController
+router.get('/:id', getMakulById)
 
 module.exports = router
